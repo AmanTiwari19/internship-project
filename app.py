5c6dbbcc0960639780ad3c2f81ceb52d3092dc24
Aman <aman19peppy@gmail.com>
2026-02-08 16:35:33 +0000

    added github actions

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "<h1>CI/CD Project: Step 1 Complete!</h1>"
+    return "<h1>CI/CD Project: Step 1 Complete and step2 also github actions!</h1>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
